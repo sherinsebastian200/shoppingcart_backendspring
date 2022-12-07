b53c3ec511e8cf07ca41ed079d95389ed97b9f9a
sherin200 <sherinsebastian21>
2022-12-07 19:15:02 +0530

    msg

---
+++ src/test/java/com/example/shoppingcartnew_backend/ShoppingcartnewBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shoppingcartnew_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingcartnewBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
