8c93e8fdea0931beda2511a5eaa7b673af3fc7ed
sherin200 <sherinsebastian21>
2022-12-10 21:59:49 +0530

    updates

--- src/main/java/com/example/shoppingcartnew_backend/dao/UserDao.java
+++ src/main/java/com/example/shoppingcartnew_backend/dao/UserDao.java
@@ -9,7 +9,10 @@ import java.util.List;
 
 public interface UserDao extends CrudRepository<User,Integer> {
 
-    @Query(value = "SELECT `id`, `address`, `confirmpassword`, `email`, `mobno`, `name`, `password` FROM `user` WHERE `name` LIKE %:name%",nativeQuery = true)
+    @Query(value = "SELECT `id`, `address`, `confirmpassword`, `email`, `mobno`, `name`, `password` FROM `user` WHERE `email`= :email AND `password` =:password",nativeQuery = true)
 
-    List<User> SearchUser(@Param("name")String name);
+    List<User> UserLogin(@Param("email") String email ,@Param("password") String password);
+
+    @Query(value ="SELECT `id`, `address`, `confirmpassword`, `email`, `mobno`, `name`, `password` FROM `user` WHERE `id`=:id",nativeQuery = true )
+    List<User> ViewProfile(@Param("id") Integer id);
 }
